0e8754be43446845d858181bc8046df8d633485f
slightun <xiao_guang_1988@163.com>
2015-12-03 18:22:14 +0800

    Add Example: AsynchronousDelegate

---
+++ AsynchronousDelegates/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousDelegates
+{
+    class Program
+    {
+        public static int ThreadFunc(int data, int ms)
+        {
+            Console.WriteLine("sub thread start...");
+            Thread.Sleep(ms);
+            Console.WriteLine("sub thread end...");
+            // ++data
+            return ++data;
+        }
+
+        public delegate int ThreadFuncDelegate(int data, int ms);
+
+        static void Main(string[] args)
+        {
+            ThreadFuncDelegate dl = new ThreadFuncDelegate(ThreadFunc);
+
+            IAsyncResult ar = dl.BeginInvoke(10, 1500, null, null);
+
+            // poll to check if sub-thread already finished
+            while(!ar.IsCompleted)
+            {
+                Console.WriteLine("main thread working...");
+                Thread.Sleep(50);
+            }
+
+            int result = dl.EndInvoke(ar);
+
+            Console.WriteLine("result: {0}", result);
+
+            Console.ReadLine();
+        }
+    }
+}
