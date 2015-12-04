0c33cc8edd84935647a5aaaa8063272a28129d31
slightun <xiao_guang_1988@163.com>
2015-12-04 17:59:07 +0800

    add project -- ThreadPools

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 
 ##Documents
 - [MSDN document](https://msdn.microsoft.com/en-us/library/aa287558(v=vs.71).aspx)
+- [Books- Professtional C# 4.0 and .NET 4]
 - [Windows系统的线程调度](http://blog.csdn.net/xywlpo/article/details/6831840) 线程内核对象、上下文；线程优先级的动态调整；进程、线程的亲缘性；
 - [操作系统知识回顾---进程线程模型 ](http://blog.chinaunix.net/uid-26430381-id-3746859.html) **进程状态切换**
 - [操作系统之CPU调度](http://blog.csdn.net/xiazdong/article/details/6280345)
@@ -16,14 +17,16 @@
 - [ThreadStartStop](#Thread)
 - [AsynchronousDelegates](#Thread) **Polling/Wait Handle/Asynchronous Callback(use lambda)** to check if sub-thread already finish and waiting for the result.
 - [BackgroundThread](#Thread) 
+- [TheadPools](#Thread)
 
 ----------
 
-### Thread
-[ThreadTurtorals-msdn](https://msdn.microsoft.com/en-us/library/aa645740(v=vs.71).aspx)
+### Threads, Tasks, and Synchronization
+- [ThreadTurtorals-msdn](https://msdn.microsoft.com/en-us/library/aa645740(v=vs.71).aspx)
 
-A thread is a independent stream of instructions in a program. 
+- A thread is a independent stream of instructions in a program. 
 
-A process contains resources, which include virtual memory and Window Handles, and contains at least one thread.
+- A process contains resources, which include virtual memory and Window Handles, and contains at least one thread.
 
-The process keeping running as long as at least one foreground thread is running. A thread created with **Thread** class, by default, is a foreground thread.
+- The process keeping running as long as at least one foreground thread is running. A thread created with **Thread** class, by default, is a foreground thread.
+- Thread Pools. All thread pool threads are background threads. You cannot set the priority or name of a pooled thread.
---
+++ ThreadPools/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+
+// WaitHandle的数量 <= 64
+
+public class Fibonacci
+{
+    public int Num
+    {
+        get { return _num; }
+    }
+    private int _num;
+
+    public int Result
+    {
+        get { return _result; }
+    }
+    private int _result;
+    private ManualResetEvent _doneEvent;
+
+    // constructor
+    public Fibonacci(int num, ManualResetEvent doneEvent)
+    {
+        _num = num;
+        _doneEvent = doneEvent;
+    }
+
+    // thread callback
+    public void ThreadCallback(object state)
+    {
+        int threadIndex = (int)state;
+        Console.WriteLine("thread {0} started..", threadIndex);
+        _result = Caculate(_num);
+        Console.WriteLine("thread {0} completed..", threadIndex);
+        _doneEvent.Set();
+    }
+
+    private int Caculate(int num)
+    {
+        if (num <= 1)
+        {
+            return num;
+        }
+        //Thread.Sleep(1);
+        return Caculate(num - 1) + Caculate(num - 2);
+    }
+
+}
+
+namespace ThreadPools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int fibCaculations = 10;
+            //const int fibCaculations = 65;
+
+            Fibonacci[] fibs = new Fibonacci[fibCaculations];
+            ManualResetEvent[] doneEvents = new ManualResetEvent[fibCaculations];
+            Random r = new Random();
+
+            Console.WriteLine("launching {0} tasks..", fibCaculations);
+            for (int nIdx = 0; nIdx < fibCaculations; ++nIdx)
+            {
+                doneEvents[nIdx] = new ManualResetEvent(false);
+
+                int num = r.Next(20, 30);
+                Fibonacci f = new Fibonacci(num, doneEvents[nIdx]);
+                fibs[nIdx] = f;
+
+                // ThreadPool
+                ThreadPool.QueueUserWorkItem(f.ThreadCallback, nIdx);
+            }
+
+            // WaitHandle的数量 <= 64
+            WaitHandle.WaitAll(doneEvents);
+            Console.WriteLine("Calculation completed..");
+
+            // result
+            for (int nIdx = 0; nIdx < fibCaculations; ++nIdx)
+            {
+                Fibonacci f = fibs[nIdx];
+                Console.WriteLine("Fibonacci({0}) = {1}", f.Num, f.Result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
